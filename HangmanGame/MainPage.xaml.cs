3f352f31bc458c7e10ecca23de69f386fcc3689a
Fagner <fagnergeo0@gmail.com>
2024-08-05 18:42:20 -0300

    Implementando interface do jogo e definindo binding nas viwes do xaml com as propriedades do code behind usando OnPropertyChaned

--- HangmanGame/MainPage.xaml.cs
+++ HangmanGame/MainPage.xaml.cs
@@ -1,25 +1,99 @@
-﻿namespace HangmanGame
+﻿using System.ComponentModel;
+
+namespace HangmanGame
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
-        int count = 0;
+        private string _destaque;
+        private List<char> _letters = new();
+        private string _message;
+        private string _gameStatus;
+        private string _currentImage = "game_img0.png";
+        string response = "";
+        int erros = 0;
+        int maxErros = 6;
+        List<char> guess = new();
+        List<string> words = new List<string>()
+        { 
+            "dotnet",
+            "maui",
+            "aspnet",
+            "sql",
+            "python",
+            "php",
+            "javascript",
+            "r",
+            "ruby",
+            "mvvm",
+            "oop",
+            "vscode",
+        };
 
         public MainPage()
         {
             InitializeComponent();
+            Letters.AddRange("abcdefghijklmnopqrstuvwxyz");
+            GameStatus = $"Erros: {erros} de {maxErros}";
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        public string Destaque
         {
-            count++;
+            get => _destaque;
+            set
+            {
+                _destaque = value;
+                OnPropertyChanged();
+            }
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        public List<char> Letters
+        {
+            get => _letters;
+            set
+            {
+                _letters = value;
+                OnPropertyChanged();
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string GameStatus
+        {
+            get => _gameStatus;
+            set
+            {
+                _gameStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string CurrentImage
+        {
+            get => _currentImage;
+            set 
+            {
+                _currentImage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void ResetButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
-
 }
